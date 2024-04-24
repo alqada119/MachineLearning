d0f08ed5d52d69d75a7cb20ccdcbdb3d13ad9190
ahmedtamer <alqad.a.119@isg.edu.sa>
2024-04-23 21:22:51 -0500

    Panda Learning Part 2 Data Frame

--- pandapy.py
+++ pandapy.py
@@ -27,3 +27,29 @@ series2=pd.Series([1,2,5],["USA","Egypt","Italy"])
 print(series2)
 print(series1["Italy"],series2["Italy"]) #How to access index aka key
 print(series1+series2) #Nan if doesn't match
+
+"""
+Part 2 Data Frames
+"""
+from numpy.random import randn
+np.random.seed(101) #same random
+df=pd.DataFrame(randn(5,4),["A","B","C","D","E"],["W","X","Y","Z"])
+print(df) #syntax is (data,rows,col)
+#To grab value
+print(df["W"])
+#Basically data frame is bunch of series with same index (excel)
+print(df[["W","Z"]]) #can do multiple cols
+df["New"]=df["W"]+df["Y"] #creating new column from current columns
+print(df)
+#To remove columns
+df.drop("W",axis=1,inplace=True) #axis = 1 for columns, axis=0 for row or index
+#inplace=True is not default just in case of accident
+print(df)
+df.drop("E",inplace=True)
+print(df)
+print(df.shape) #tuple (row,col) hence why row is 0 and col is 1
+#To select Rows
+print(df.loc["A"])
+print(df.iloc[2]) #basically by number indexing
+print(df.loc["A","X"]) #row,col
+print(df.loc["A"]["X"]) #This also works
