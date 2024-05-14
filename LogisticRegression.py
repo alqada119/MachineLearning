2b6961c17da226c9e08518c0095557fc9cd36120
ahmedtamer <alqad.a.119@isg.edu.sa>
2024-05-15 00:59:36 +0300

    LOGISTIC REGRESSION: CLeaning data, and filling na age based on good guess. Dropping Column for missing so much data.

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -17,4 +17,29 @@ test=pd.read_csv("titanic_test.csv")
 # print(train.info())
 # # sns.countplot(x="SibSp",data=train)
 # sns.displot(x="Fare",data=train)
+# plt.show()
+"""
+Fill in missing data
+"""
+print(train.describe())
+# print(train.info())
+sns.boxplot(x="Pclass",y="Age",data=train)
+def impute_age(cols):
+    Age = cols[0]
+    Pclass=cols[1]
+    if pd.isnull(Age):
+        if Pclass==1:
+            return 37
+        elif Pclass==2:
+            return 29
+        else:
+            return 24
+    else:
+        return Age
+train["Age"]=train[["Age","Pclass"]].apply(impute_age,axis=1) #.apply takes the [] as param
+"""
+Since Cabin column is missing so much info drop col
+"""
+train.drop("Cabin",axis=1,inplace=True)
+sns.heatmap(train.isnull(),yticklabels=False,cbar=False,cmap="viridis")
 plt.show()
